a75bda89a796715fe68b3f667bba80b7e21fee24
Artur <Artur@LAPTOP-7VQCC982>
2023-04-15 21:45:54 +0500

    Добавьте файлы проекта.

---
+++ Homework14.04.23/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework14._04._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Usage: ToCelsius.exe <temperature in F>");
+                return;
+            }
+
+            double fahrenheit;
+            if (!double.TryParse(args[0], out fahrenheit))
+            {
+                Console.WriteLine("Invalid input: {0}", args[0]);
+                return;
+            }
+
+            double celsius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine("{0} F = {1} C", fahrenheit, celsius);
+
+        }
+    }
+}
